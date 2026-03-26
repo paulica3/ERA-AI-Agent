320470523e3a2a2d51994960d5d60323b8f7e827
Paul Efrim <paoloefrim7@gmail.com>
2026-03-26 10:31:25 +0100

    first prototype

---
+++ ERA AI/Pages/LogIn.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERA_AI.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
